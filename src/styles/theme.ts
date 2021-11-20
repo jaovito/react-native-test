586be688df8d43f4ccd2afba006b0adad0f6ae50
jaovito <junior.vitorio.dias@gmail.com>
2021-11-20 10:16:23 -0300

    add gray color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,9 @@ const theme = {
     linear01: '#009EA0',
     linear02: '#006DA5',
     background: '#FEFEFE',
+    sectionHeader: '#005D95',
     white: '#E8F5F6',
+    gray: '#C9C7CD',
     text: {
       white: '#E8F5F6',
       black: '#606060',
