f524b602003a5c21f88dc8d8f9459c49beb94711
jaovito <junior.vitorio.dias@gmail.com>
2021-11-20 08:15:50 -0300

    add temas globais

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    linear01: "#009EA0",
+    linear02: "#006DA5",
+    background: "#FEFEFE",
+    text: {
+      white: "#E8F5F6",
+      black: "#606060"
+    }
+  } 
+}
+
+export default theme;
